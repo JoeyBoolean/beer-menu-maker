62320f7d0ac6ce930a741dd8e56eb72494e88341
Joey Bachand <bachand.joey@gmail.com>
2014-04-08 22:46:24 -0500

    template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeerMenuMaker::Application.config.secret_key_base = 'cfa772cc08a954a13d664c9ab77fc3e825219daf25c4377b9f8cbf6d6717c2eadee74a598705a83e99d292037c3e24ece932f81562e7b8d98f5a6cb53195b441'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeerMenuMaker::Application.config.session_store :cookie_store, key: '_beer-menu-maker_session'
